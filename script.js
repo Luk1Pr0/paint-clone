1de82a168218201165276a49b8828b15f54bccaa
Luk1Pr0 <lpytel16@gmail.com>
2020-12-18 11:51:41 +0100

    Brush timeout function created

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const BRUSH_TIME = 1500;
 const activeToolEl = document.getElementById('active-tool');
 const brushColorBtn = document.getElementById('brush-color');
 const brushIcon = document.getElementById('brush');
@@ -73,6 +74,11 @@ function switchToBrush() {
   displayBrushSize();
 }
 
+// Set timeout to switch back to the brush
+function brushTimeout(ms) {
+  setTimeout(switchToBrush, ms);
+}
+
 // Create Canvas
 function createCanvas() {
   canvas.width = window.innerWidth;
@@ -89,7 +95,7 @@ clearCanvasBtn.addEventListener('click', () => {
   drawnArray = [];
   // Active Tool
   activeToolEl.textContent = 'Canvas Cleared';
-  setTimeout(switchToBrush, 1500);
+  setTimeout(switchToBrush, BRUSH_TIME);
 });
 
 // Draw what is stored in DrawnArray
@@ -169,7 +175,7 @@ saveStorageBtn.addEventListener('click', () => {
   localStorage.setItem('savedCanvas', JSON.stringify(drawnArray));
   // Active Tool
   activeToolEl.textContent = 'Canvas Saved';
-  setTimeout(switchToBrush, 1500);
+  brushTimeout(BRUSH_TIME);
 });
 
 // Load from Local Storage
@@ -179,7 +185,7 @@ loadStorageBtn.addEventListener('click', () => {
     restoreCanvas();
     // Active Tool
     activeToolEl.textContent = 'Canvas Loaded';
-    setTimeout(switchToBrush, 1500);
+    brushTimeout(BRUSH_TIME);
   } else {
     activeToolEl.textContent = 'Could not load canvas - no saved canvas found';
   }
@@ -191,7 +197,7 @@ clearStorageBtn.addEventListener('click', () => {
   localStorage.removeItem('savedCanvas');
   // Active Tool
   activeToolEl.textContent = 'Local Storage Cleared';
-  setTimeout(switchToBrush, 1500);
+  brushTimeout(BRUSH_TIME);
 });
 
 // Download Image
@@ -200,7 +206,7 @@ downloadBtn.addEventListener('click', () => {
   downloadBtn.download = 'drawing.jpeg';
   // Active Tool
   activeToolEl.textContent = 'Image File Saved';
-  setTimeout(switchToBrush, 1500);
+  brushTimeout(BRUSH_TIME);
 });
 
 // Event Listener
